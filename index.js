d8cdc7ac10038cd9efec59e9601fdae6e9ac6270
KasoHodaj <kasohodaj@yahoo.gr>
2023-12-12 15:52:19 +0200

    update 12/12/23

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 var btnLength = document.querySelectorAll(".piece").length;
-
+var isPlaying = false;
 for (var i = 0; i < btnLength; i++) {
-    document.querySelectorAll(".piece")[i].addEventListener("click", function(){
+    document.querySelectorAll(".piece")[i].addEventListener("click", function () {
         var buttonInnerHtml = this.innerHTML;
-        audioPlay(buttonInnerHtml);  
+        
+        if(!isPlaying){
+            audioPlay(buttonInnerHtml);
+            buttonAnimation(buttonInnerHtml);
+        }        
     });
 }
+ 
 
-document.addEventListener('keypress', function (event) {
-    audioPlay(event.key);
+
+function audioPlay(key) {
     
-});
-
-function audioPlay(key){
     switch (key) {
-        case "1":
+        case "one":
             var audio1 = new Audio('Sound/1.mp3');
             audio1.play();
+            isPlaying = true;
             break;
-        case "2":
+        case "two":
             var audio2 = new Audio('Sound/2.mp3');
             audio2.play();
+            isPlaying = true;
             break;
-        case "3":
-            audio3 = new Audio('Sound/3.mp3');
+        case "three":
+            var audio3 = new Audio('Sound/3.mp3'); // Declare 'audio3' using 'var'
             audio3.play();
+            isPlaying = true;
             break;
-        case "4":
-            audio4 = new Audio('Sound/4.mp3');
+        case "four":
+            var audio4 = new Audio('Sound/4.mp3'); // Declare 'audio4' using 'var'
             audio4.play();
+            isPlaying = true;
             break;
         default:
-            console.log(buttonInnerHtml);
+            console.log("Unhandled key: " + key);
     }
 }
 
+function buttonAnimation(key) {
+    var AB = document.querySelector("." + key);
+    AB.classList.add("pressed");
+
+   
+}
